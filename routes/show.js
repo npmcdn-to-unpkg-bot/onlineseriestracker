b0c4e3b371be59cb82845f54deb66ea97685f0c6
Schavras <stavros.z@hotmail.com>
2016-08-24 02:49:05 +0300

    Test

--- routes/show.js
+++ routes/show.js
@@ -1,3 +1,10 @@
 /**
  * Created by stavr on 24/8/2016.
  */
+
+var express = require ('express');
+var router = express.Router();
+
+router.get('/', function(req,res,next){
+    console.log("klmr");
+});
